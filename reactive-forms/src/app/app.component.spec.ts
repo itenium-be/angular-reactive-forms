7cad6521a92aeba8adbf4d340dea7cd6824e0177
Wouter Van Schandevijl <woutervs@hotmail.com>
2023-01-16 15:41:02 +0100

    fix component unittests: fake TranslateService

--- reactive-forms/src/app/app.component.spec.ts
+++ reactive-forms/src/app/app.component.spec.ts
@@ -1,6 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateService } from '@ngx-translate/core';
+import { of } from 'rxjs';
 import { AppComponent } from './app.component';
+
+class TranslateServiceStub {
+  get(key: any): any {
+    return of(key);
+  }
+
+  setDefaultLang = () => {};
+  use = () => {};
+}
+
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -11,6 +23,9 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      providers: [
+        {provide: TranslateService, useClass: TranslateServiceStub}
+      ]
     }).compileComponents();
   });
 
