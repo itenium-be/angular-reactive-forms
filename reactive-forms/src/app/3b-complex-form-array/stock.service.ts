bd28f6c0eb3c793955e7f0950819b99d6a7cddc9
Wouter Van Schandevijl <woutervs@hotmail.com>
2024-12-06 11:22:39 +0100

    more type safety 😃

--- reactive-forms/src/app/3b-complex-form-array/stock.service.ts
+++ reactive-forms/src/app/3b-complex-form-array/stock.service.ts
@@ -3,13 +3,13 @@ import { Observable, of } from 'rxjs';
 
 export const Colors = ['Red', 'Green', 'Yellow', 'Blue'] as const;
 
-type ElementType <T extends ReadonlyArray<unknown>> = T extends ReadonlyArray<infer ElementType>
+type ElementType<T extends ReadonlyArray<unknown>> = T extends ReadonlyArray<infer ElementType>
   ? ElementType
   : never
 
 export type ColorsType = ElementType<typeof Colors>
 
-const stock: {[key: string]: number} = {
+const stock: Record<ColorsType, number> = {
   Red: 5,
   Green: 2,
   Yellow: 9,
@@ -18,7 +18,7 @@ const stock: {[key: string]: number} = {
 
 @Injectable({ providedIn: 'root' })
 export class StockService {
-  getStock(color: string): Observable<number> {
+  getStock(color: ColorsType): Observable<number> {
     const colorStock = stock[color];
     return of(colorStock || 0);
   }
